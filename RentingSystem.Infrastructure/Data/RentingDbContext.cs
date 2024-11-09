dc591ad643b6b1897e7fa6ced446b1b01320f3a4
IvanAbadzhiev01 <ivan087611@gmail.com>
2024-11-09 15:59:08 +0200

    Program.cs cleared

---
+++ RentingSystem.Infrastructure/Data/RentingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentingSystem.Infrastructure.Data
+{
+    public class RentingDbContext : IdentityDbContext<IdentityUser>
+    {
+        public RentingDbContext(DbContextOptions<RentingDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ RentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RentingSystem.Data;
+using RentingSystem.Infrastructure.Data;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services
+                .AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.User.RequireUniqueEmail = true;
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<RentingDbContext>();
+
+            return services;
+        }
+
+    }
+}
