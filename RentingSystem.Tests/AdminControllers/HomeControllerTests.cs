be79f31c39b1f00041dbcf62a5be51f999c6ed1d
IvanAbadzhiev01 <ivan087611@gmail.com>
2024-12-03 22:25:52 +0200

    Admin Home test

---
+++ RentingSystem.Tests/AdminControllers/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using RentingSystem.Areas.Admin.Controllers;
+
+[TestFixture]
+public class HomeControllerTests
+{
+    private HomeController _controller;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _controller = new HomeController();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _controller.Dispose();
+    }
+
+    [Test]
+    public void Dashboard_ShouldReturnViewResult()
+    {
+        var result = _controller.Dashboard();
+
+        Assert.That(result, Is.TypeOf<ViewResult>());
+    }
+}
